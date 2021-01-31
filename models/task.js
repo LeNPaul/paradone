6976794060a6f8320fb337d6684ad9acc8457594
Paul Le <l.nguyen.paul@gmail.com>
2021-01-31 15:11:37 -0500

    #7 Update database schema for tasks

--- models/task.js
+++ models/task.js
@@ -3,10 +3,17 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var Task = new Schema({
-    username:  String,
-    content:   String,
-    due:       Date,
-    completed: Boolean
+  username:     String,
+  content:      String,
+  id:           String,
+  project_id:   String,
+  section_id:   String,
+  parent_id:    String,
+  label_ids:    Array,
+  priority:     Integer,
+  order:        Integer,
+  due_datetime: Date,
+  completed:    Boolean
 });
 
 Task.plugin(passportLocalMongoose);
