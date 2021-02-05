6d9d80269cc17d4a94ff76e44c50c39f67d6280f
Paul Le <l.nguyen.paul@gmail.com>
2021-02-05 01:21:29 -0500

    #11 Add API request to update task

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -5,22 +5,23 @@ var app = new Vue({
     taskText: null
   },
   created () {
-    this.loadTasks();
+    this.loadTasks()
   },
   methods: {
     loadTasks: function() {
       axios
-        .get('/tasks').then(response =>  this.tasks = response.data);
+        .get('/tasks').then(response =>  this.tasks = response.data)
     },
     addTask: function() {
       if (this.taskText != null) {
         axios
           .post('/tasks', {username: 'paul', content: this.taskText})
-        this.taskText = null;
+        this.taskText = null
       }
     },
     completeTask: function() {
-      console.log('Hello world!');
+      axios
+        .put('/tasks', {username: 'paul', task_id:'ecf9af29-5ca5-473d-ae0b-599fcd2f18df', completed: true})
     }
   }
 })
