7a8be71ad7c800928b305668ace89918057d579b
Paul Le <l.nguyen.paul@gmail.com>
2021-10-26 23:07:54 -0400

    #36 Add Task and Tasks component

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="container">
     <Header title="Tasks"/>
+    <Tasks :tasks="tasks"/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'test'
+      },
+      {
+        id: 2,
+        text: 'hello'
+      }
+    ]
   }
 }
+
 </script>
 
 <style>
