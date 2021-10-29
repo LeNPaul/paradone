36cfafb3051a00add40ec0e9693e912cd1bc7d1f
Paul Le <l.nguyen.paul@gmail.com>
2021-10-29 16:01:27 -0400

    #36 Add sidebar starting point

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="col-lg-6 mx-auto p-3 py-md-5">
-    <Header @toggle-add-task="toggleAddTask" title="Tasks" :showAddTask="showAddTask"/>
-    <router-view :showAddTask="showAddTask"></router-view>
-    <Footer/>
+  <Sidebar/>
+  <div class="container-fluid">
+      <div class="row">
+          <div class="col min-vh-100 p-4">
+              <!-- toggler -->
+              <button class="btn float-left" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" role="button">
+                  <i class="fas fa-bars" data-bs-toggle="offcanvas" data-bs-target="#offcanvas"></i>
+              </button>
+              <div class="col-lg-6 mx-auto p-3 py-md-5">
+                <Header @toggle-add-task="toggleAddTask" title="Tasks" :showAddTask="showAddTask"/>
+                <router-view :showAddTask="showAddTask"></router-view>
+                <Footer/>
+              </div>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
+import Sidebar from './components/Sidebar'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
+    Sidebar,
     Header,
     Footer
   },
