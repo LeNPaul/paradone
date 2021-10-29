4358c7b493fa91adc26cdbbd19dfc1ac82acba23
Paul Le <l.nguyen.paul@gmail.com>
2021-10-29 10:33:49 -0400

    #36 Remove unused code and change text to content

--- todo-app/src/components/AddTask.vue
+++ todo-app/src/components/AddTask.vue
@@ -2,13 +2,8 @@
   <form @submit="onSubmit" class="add-form">
     <div class="form-control">
       <label>Task</label>
-      <input type="text" v-model="text" name="text" placeholder="Add Task" />
+      <input type="text" v-model="content" name="content" placeholder="Add Task" />
     </div>
-    <div class="form-control form-control-check">
-      <label>Set Reminder</label>
-      <input type="checkbox" v-model="reminder" name="reminder" />
-    </div>
-
     <input type="submit" value="Save Task" class="btn btn-block" />
   </form>
 </template>
@@ -18,58 +13,25 @@
     name: 'AddTask',
     data() {
       return {
-        text: '',
-        day: '',
-        reminder: false
+        content: '',
       }
     },
     methods: {
       onSubmit(e) {
         e.preventDefault()
-        if(!this.text) {
+        if(!this.content) {
           alert('Please add a task')
           return
         }
         const newTask = {
-          text: this.text,
-          reminder: this.reminder
+          content: this.content,
         }
         this.$emit('add-task', newTask)
-        this.text = ''
-        this.day = ''
-        this.reminder = false
+        this.content = ''
       }
     }
   }
 </script>
 
 <style scoped>
-.add-form {
-  margin-bottom: 40px;
-}
-.form-control {
-  margin: 20px 0;
-}
-.form-control label {
-  display: block;
-}
-.form-control input {
-  width: 100%;
-  height: 40px;
-  margin: 5px;
-  padding: 3px 7px;
-  font-size: 17px;
-}
-.form-control-check {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-.form-control-check label {
-  flex: 1;
-}
-.form-control-check input {
-  flex: 2;
-  height: 20px;
-}
 </style>
--- todo-app/src/components/Task.vue
+++ todo-app/src/components/Task.vue
@@ -1,7 +1,7 @@
 <template>
   <p class="pb-3 border-bottom">
     <button class="btn btn-link link-dark" type="submit" v-on:click="completeTask(task.task_id)"><i @click="$emit('delete-task', task.id)" class="far fa-circle"></i></button>
-    {{ task.text }}
+    {{ task.content }}
   </p>
 </template>
 
