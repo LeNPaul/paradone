62049272deb7e738bdcf033ee79db260276df8de
Paul Le <l.nguyen.paul@gmail.com>
2021-10-30 16:55:16 -0400

    #36 Add ability to edit task

---
+++ todo-app/src/components/EditTask.vue
@@ -0,0 +1,102 @@
+<template>
+  <form @submit="onSubmit" class="form-control add-form mb-3 mt-3 p-3">
+    <div class="mb-3">
+      <input type="text" v-model="content" name="content" placeholder="Task content" class="form-control">
+    </div>
+    <button type="submit" class="btn btn-dark">Save</button>
+    <button class="btn btn-outline-dark btn-sm float-end" type="button" data-bs-toggle="dropdown"><i class="fas fa-tags"></i> {{ label }} </button>
+    <ul class="dropdown-menu">
+        <li :key='label.label_id' v-for="label in labels" @click="this.label=label.name"><a class="dropdown-item">{{ label.name }}</a></li>
+    </ul>
+    <button class="btn btn-outline-dark btn-sm float-end me-2" type="button" data-bs-toggle="dropdown"><i class="far fa-flag"> </i> {{ priority }} </button>
+    <ul class="dropdown-menu">
+        <li><a class="dropdown-item" @click="this.priority='1'">Priority 1</a></li>
+        <li><a class="dropdown-item" @click="this.priority='2'">Priority 2</a></li>
+        <li><a class="dropdown-item" @click="this.priority='3'">Priority 3</a></li>
+        <li><a class="dropdown-item" @click="this.priority='4'">Priority 4</a></li>
+    </ul>
+    <button class="btn btn-outline-dark btn-sm float-end me-2" type="button" data-bs-toggle="dropdown"><i class="fas fa-tasks"> </i> {{ project }} </button>
+    <ul class="dropdown-menu">
+        <li :key='project.name' v-for="project in projects" @click="this.project=project.name"><a class="dropdown-item">{{ project.name }}</a></li>
+    </ul>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'AddTask',
+    data() {
+      return {
+        content: '',
+        label: '',
+        labels: [],
+        priority: '',
+        project: '',
+        projects: []
+      }
+    },
+    props: {
+      task: Object
+    },
+    methods: {
+      async onSubmit(e) {
+        e.preventDefault()
+        if(!this.content) {
+          alert('Please add a task')
+          return
+        }
+        const taskToUpdate = await this.fetchTask(this.$props.task.id)
+        const updTask = {
+          ...taskToUpdate,
+          content: this.content,
+          label: this.label,
+          priority: this.priority,
+          project: this.project
+        }
+        const res = await fetch(`/api/tasks/${this.$props.task.id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify(updTask),
+        })
+        const data = await res.json()
+        return data
+      },
+      async fetchTask(id) {
+        const res = await fetch(`/api/tasks/${id}`)
+        const data = await res.json()
+        return data
+      },
+      async fetchProjects() {
+        const res = await fetch('/api/projects')
+        const data = await res.json()
+        return data
+      }
+    },
+    async created() {
+      this.content = this.$props.task.content
+      this.label = this.$props.task.label
+      this.priority = this.$props.task.priority
+      this.project = this.$props.task.project
+      this.projects = await this.fetchProjects()
+      this.labels = [
+        {
+          label_id: 'todo',
+          name: 'To Do'
+        },
+        {
+          label_id: 'doing',
+          name: 'Doing'
+        },
+        {
+          label_id: 'done',
+          name: 'Done'
+        }
+      ]
+    }
+  }
+</script>
+
+<style scoped>
+</style>
--- todo-app/src/components/Task.vue
+++ todo-app/src/components/Task.vue
@@ -2,14 +2,31 @@
   <p class="pb-3 border-bottom">
     <button class="btn btn-link link-dark" type="submit"><i @click="$emit('delete-task', task.id)" class="far fa-circle"></i></button>
     {{ task.content }}
+    <button class="btn btn-link link-dark float-end" type="submit"><i @click="toggleEditTask()" class="fas fa-edit"></i></button>
+    <EditTask v-show="showEditTask" :task="task"/>
   </p>
 </template>
 
 <script>
+import EditTask from './EditTask'
+
 export default {
   name: 'Task',
   props: {
     task: Object
+  },
+  components: {
+    EditTask
+  },
+  data() {
+    return {
+      showEditTask: false
+    }
+  },
+  methods: {
+    toggleEditTask() {
+      this.showEditTask = !this.showEditTask
+    },
   }
 }
 </script>
