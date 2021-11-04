20270a49a6c9eb1495cc15793c9908729806b9e7
Paul Le <l.nguyen.paul@gmail.com>
2021-11-03 23:02:23 -0400

    #38 Add initial backend server code and route for projects

---
+++ todo-app/server/server/routes/api/projects.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get Projects
+router.get('/', async (req, res) => {
+  const projects = await loadProjectsCollection();
+  res.send(await projects.find({}).toArray());
+});
+
+// Add Project
+router.post('/', async (req, res) => {
+  const projects = await loadProjectsCollection();
+  await projects.insertOne({
+    userName:    req.body.userName,
+    projectName: req.body.projectName,
+    projectId:   req.body.projectId,
+    colour:      req.body.colour,
+    order:       req.body.order,
+    archived:    req.body.archived
+  });
+  res.status(201).send();
+});
+
+// Delete Project
+router.delete('/:id', async (req, res) => {
+  const projects = await loadProjectsCollection();
+  await projects.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+  res.status(200).send({});
+});
+
+async function loadProjectsCollection() {
+  const client = await mongodb.MongoClient.connect(
+    'mongodb://localhost', {
+    useNewUrlParser: true
+  });
+  return client.db('todo-app-backend').collection('projects');
+}
+
+module.exports = router;
