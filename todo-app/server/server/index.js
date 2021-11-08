c1a3546e6e37931cf8e2b5d0ae2744920b00d1a7
Paul Le <l.nguyen.paul@gmail.com>
2021-11-07 21:35:50 -0500

    #38 Add morgan http request logger

--- todo-app/server/server/index.js
+++ todo-app/server/server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan('dev'));
 
 // Routes
 const tasks = require('./routes/api/tasks');
