451e775dce4994660db7d3610b3a0f6691a84e61
Paul Le <l.nguyen.paul@gmail.com>
2021-11-07 21:45:26 -0500

    #38 Update task.id to task._id

--- todo-app/client/src/components/Workspace.vue
+++ todo-app/client/src/components/Workspace.vue
@@ -48,7 +48,7 @@ export default {
         method: 'DELETE',
       })
       res.status === 200
-        ? (this.tasks = this.tasks.filter((task) => task.id !== id))
+        ? (this.tasks = this.tasks.filter((task) => task._id !== id))
         : alert('Error deleting task')
     },
     async fetchTasks() {
