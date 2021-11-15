ef6facc0af066d138401e9b799f6c4de4e42148a
Paul Le <l.nguyen.paul@gmail.com>
2021-11-14 22:38:42 -0500

    #37 Change Users schema to User, add users route for registering a new user, and use mongoose to connect to database

--- todo-app/server/index.js
+++ todo-app/server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const config = require('config');
 
 const app = express();
 
@@ -10,16 +12,26 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('dev'));
 
+// DB Config
+const db = config.get('mongoURI');
+
+// Connect to MongoDB
+mongoose.connect(db)
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
 // Routes
 const tasks = require('./routes/api/tasks');
 const projects = require('./routes/api/projects');
 const labels = require('./routes/api/labels');
 const auth = require('./routes/api/auth');
+const users = require('./routes/api/users');
 
 app.use('/api/tasks', tasks);
 app.use('/api/projects', projects);
 app.use('/api/labels', labels);
 app.use('/api/auth', auth);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 
