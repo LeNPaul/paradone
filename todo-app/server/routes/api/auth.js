1c398c62c272327c520064e7c226812a98a93b5f
Paul Le <l.nguyen.paul@gmail.com>
2021-11-14 23:16:28 -0500

    #37 Add middleware for authenticating routes

--- todo-app/server/routes/api/auth.js
+++ todo-app/server/routes/api/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const config = require('config');
 const jwt = require('jsonwebtoken');
+const auth = require('../../middleware/auth')
 
 // User Model
 const User = require('../../models/User');
@@ -51,5 +52,14 @@ router.post('/', (req, res) => {
     });
 
 });
+
+// @route  Get api/auth/user
+// @desc   Get user data
+// @access Private
+router.get('/user', auth, (req, res) => {
+  User.findById(req.user.id)
+    .select('-password')
+    .then(user => res.json(user));
+})
 
 module.exports = router;
