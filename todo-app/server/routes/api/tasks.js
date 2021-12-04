3607bfffeacb650d502ff36a4ecdf6fcbe8da9ca
Paul Le <l.nguyen.paul@gmail.com>
2021-12-03 23:55:48 -0500

    #22 Add encrypt and decrypt function

--- todo-app/server/routes/api/tasks.js
+++ todo-app/server/routes/api/tasks.js
@@ -3,6 +3,60 @@ const router = express.Router();
 const mongodb = require('mongodb');
 const auth = require('../../middleware/auth')
 
+
+// Node.js program to demonstrate the
+// crypto.createDecipheriv() method
+
+// Includes crypto module
+const crypto = require('crypto');
+
+// Difining algorithm
+const algorithm = 'aes-256-cbc';
+
+// Defining key
+const key = crypto.randomBytes(32);
+
+// Defining iv
+const iv = crypto.randomBytes(16);
+
+// An encrypt function
+function encrypt(text) {
+
+ // Creating Cipheriv with its parameter
+ let cipher =
+    crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+
+ // Updating text
+ let encrypted = cipher.update(text);
+
+ // Using concatenation
+ encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+ // Returning iv and encrypted data
+ return { iv: iv.toString('hex'),
+     encryptedData: encrypted.toString('hex') };
+}
+
+// A decrypt function
+function decrypt(text) {
+
+ let iv = Buffer.from(text.iv, 'hex');
+ let encryptedText =
+    Buffer.from(text.encryptedData, 'hex');
+
+ // Creating Decipher
+ let decipher = crypto.createDecipheriv(
+        'aes-256-cbc', Buffer.from(key), iv);
+
+ // Updating encrypted text
+ let decrypted = decipher.update(encryptedText);
+ decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+ // returns data after decryption
+ return decrypted.toString();
+}
+
+
 // Task model
 const Task = require('../../models/Task');
 
