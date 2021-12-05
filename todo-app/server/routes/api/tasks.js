a34e52752aba97fc7060dce8d782678005bc92cd
Paul Le <l.nguyen.paul@gmail.com>
2021-12-04 19:31:03 -0500

    #22 Change encrypt and decrypt function to use crypto-js

--- todo-app/server/routes/api/tasks.js
+++ todo-app/server/routes/api/tasks.js
@@ -1,61 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const mongodb = require('mongodb');
-const auth = require('../../middleware/auth')
+const CryptoJS = require('crypto-js');
+const auth = require('../../middleware/auth');
 
+let secret = 'keyboardcat123'
 
-// Node.js program to demonstrate the
-// crypto.createDecipheriv() method
-
-// Includes crypto module
-const crypto = require('crypto');
-
-// Difining algorithm
-const algorithm = 'aes-256-cbc';
-
-// Defining key
-const key = crypto.randomBytes(32);
-
-// Defining iv
-const iv = crypto.randomBytes(16);
-
-// An encrypt function
-function encrypt(text) {
-
- // Creating Cipheriv with its parameter
- let cipher =
-    crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
-
- // Updating text
- let encrypted = cipher.update(text);
-
- // Using concatenation
- encrypted = Buffer.concat([encrypted, cipher.final()]);
-
- // Returning iv and encrypted data
- return { iv: iv.toString('hex'),
-     encryptedData: encrypted.toString('hex') };
+function encrypt(plainText) {
+  return CryptoJS.AES.encrypt(plainText, secret).toString();
 }
 
-// A decrypt function
-function decrypt(text) {
-
- let iv = Buffer.from(text.iv, 'hex');
- let encryptedText =
-    Buffer.from(text.encryptedData, 'hex');
-
- // Creating Decipher
- let decipher = crypto.createDecipheriv(
-        'aes-256-cbc', Buffer.from(key), iv);
-
- // Updating encrypted text
- let decrypted = decipher.update(encryptedText);
- decrypted = Buffer.concat([decrypted, decipher.final()]);
-
- // returns data after decryption
- return decrypted.toString();
+function decrypt(cipherText) {
+  return CryptoJS.AES.decrypt(cipherText.toString(), secret.toString(CryptoJS.enc.Utf8);
 }
-
 
 // Task model
 const Task = require('../../models/Task');
