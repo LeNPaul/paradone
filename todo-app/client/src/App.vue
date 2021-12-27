8fedf74ff1d0634e7309251288afa32aa038df64
Paul Le <l.nguyen.paul@gmail.com>
2021-12-26 22:59:44 -0500

    #40 Push to app dashboard if user logged in and on the home page, login, or register page

--- todo-app/client/src/App.vue
+++ todo-app/client/src/App.vue
@@ -42,6 +42,9 @@ export default {
   watch:{
     $route (){
       this.isLoggedIn = this.$store.getters.isLoggedIn
+      if(this.isLoggedIn && (this.$route.path == '/' || this.$route.path == '/login' || this.$route.path == '/register')) {
+        this.$router.push('/app')
+      }
     }
   }
 }
