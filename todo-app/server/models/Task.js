79aea12316b5ba355eea76e8d3739d3517a5304d
Paul Le <l.nguyen.paul@gmail.com>
2021-12-28 13:32:50 -0500

    #41 Update task schema

--- todo-app/server/models/Task.js
+++ todo-app/server/models/Task.js
@@ -2,23 +2,39 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Create Schema
+// Use the document ID as the task_id
 const TaskSchema = new Schema({
-  user_id: {
-    type: String,
-    required: true
-  },
   content: {
     type: String,
     required: true
   },
-  label: {
+  user_id: {
+    type: String,
+    required: true
+  },
+  project_id: {
     type: String
+  },
+  section_id: {
+    type: String
+  },
+  parent_id: {
+    type: String
+  },
+  label_ids: {
+    type: Array
   },
   priority: {
     type: String
   },
-  project_name: {
-    type: String
+  order: {
+    type: Number
+  },
+  due_datetime: {
+    type: Date
+  },
+  completed: {
+    type: Boolean
   }
 });
 
