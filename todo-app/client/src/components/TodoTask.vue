ed9dead41a3187f282f41f8a17927fe4d5bc4199
Paul Le <l.nguyen.paul@gmail.com>
2021-12-28 20:56:20 -0500

    #34 Change TaskEdit to WorkspaceTaskEdit

--- todo-app/client/src/components/TodoTask.vue
+++ todo-app/client/src/components/TodoTask.vue
@@ -3,12 +3,12 @@
     <button class="btn btn-link link-dark" type="submit"><i @click="$emit('delete-task', task._id)" class="far fa-circle"></i></button>
     {{ task.content }}
     <button class="btn btn-link link-dark float-end" type="submit"><i @click="toggleEditTask()" class="fas fa-edit"></i></button>
-    <EditTask @update-task="$emit('update-task')" @close-edit-task="toggleEditTask()" v-show="showEditTask" :task="task"/>
+    <WorkspaceTaskEdit @update-task="$emit('update-task')" @close-edit-task="toggleEditTask()" v-show="showEditTask" :task="task"/>
   </p>
 </template>
 
 <script>
-import EditTask from './EditTask'
+import WorkspaceTaskEdit from './WorkspaceTaskEdit'
 
 export default {
   name: 'TodoTask',
@@ -19,7 +19,7 @@ export default {
     }
   },
   components: {
-    EditTask
+    WorkspaceTaskEdit
   },
   emits: ['update-task', 'delete-task', 'close-edit-task'],
   data() {
--- todo-app/client/src/components/WorkspaceTaskEdit.vue
+++ todo-app/client/src/components/WorkspaceTaskEdit.vue
@@ -24,7 +24,7 @@
 
 <script>
   export default {
-    name: 'EditTask',
+    name: 'WorkspaceTaskEdit',
     data() {
       return {
         content: '',
