e11abf546cf67952b2bf58344f5970cb6328bdc4
Paul Le <l.nguyen.paul@gmail.com>
2022-01-03 01:25:01 -0500

    #43 Add WorkspaceParadigmKanbanCard component

---
+++ todo-app/client/src/components/WorkspaceParadigmKanbanCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card mb-3 px-3">
+    <div class="card-body">
+      <button class="btn btn-link link-dark" type="submit"><i @click="$emit('delete-task', task._id)" class="far fa-circle"></i></button>
+      {{ task.content }}
+      <button class="btn btn-link link-dark float-end" type="submit"><i @click="toggleEditTask()" class="fas fa-edit"></i></button>
+    </div>
+    <WorkspaceTaskEdit @update-task="$emit('update-task')" @close-edit-task="toggleEditTask()" v-show="showEditTask" :task="task"/>
+  </div>
+</template>
+
+<script>
+import WorkspaceTaskEdit from './WorkspaceTaskEdit'
+
+export default {
+  name: 'WorkspaceParadigmKanbanCard',
+  props: {
+    task: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    WorkspaceTaskEdit
+  },
+  emits: ['update-task', 'delete-task', 'close-edit-task'],
+  data() {
+    return {
+      showEditTask: false
+    }
+  },
+  methods: {
+    toggleEditTask() {
+      this.showEditTask = !this.showEditTask
+    },
+  }
+}
+</script>
