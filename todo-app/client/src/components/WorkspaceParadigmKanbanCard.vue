317582599e975de18e5f535ac0860c58f2cc94f8
Paul Le <l.nguyen.paul@gmail.com>
2022-01-04 22:21:57 -0500

    #42 Update Kanban card component to include project and priority when it exists

--- todo-app/client/src/components/WorkspaceParadigmKanbanCard.vue
+++ todo-app/client/src/components/WorkspaceParadigmKanbanCard.vue
@@ -3,6 +3,8 @@
     <div class="card-body d-inline-block text-truncate" style="max-width: 280px;">
       <button class="btn btn-link link-dark" type="submit"><i @click="$emit('delete-task', task._id)" class="far fa-circle"></i></button>
       {{ task.content }}
+      <button v-show="project_name" type="button" class="btn btn-outline-dark p-1 ms-2" disabled>{{ project_name }}</button>
+      <button v-show="task.priority" type="button" class="btn btn-outline-dark p-1 ms-2 px-2" disabled>{{ task.priority }}</button>
       <button class="btn btn-link link-dark float-end" type="submit"><i @click="toggleEditTask()" class="fas fa-edit"></i></button>
     </div>
     <WorkspaceTaskEdit @update-task="$emit('update-task')" @close-edit-task="toggleEditTask()" v-show="showEditTask" :task="task"/>
@@ -26,13 +28,37 @@ export default {
   emits: ['update-task', 'delete-task', 'close-edit-task'],
   data() {
     return {
-      showEditTask: false
+      showEditTask: false,
+      projects: [],
+      project_name: ''
     }
   },
   methods: {
     toggleEditTask() {
       this.showEditTask = !this.showEditTask
     },
+    async fetchProjects() {
+      const res = await fetch('/api/projects', {
+        headers: {
+          'x-auth-token': localStorage.getItem('token') || ''
+        }
+      })
+      const data = await res.json()
+      return data
+    },
+    async resetTask() {
+      this.projects = await this.fetchProjects()
+      let project = this.projects.find(o => o._id === this.$props.task.project_id)
+      this.project_name = project ? project.project_name : ''
+    }
+  },
+  watch: {
+    task: function() {
+      this.resetTask()
+    }
+  },
+  created() {
+    this.resetTask()
   }
 }
 </script>
