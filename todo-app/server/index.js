8bcf4a5423990802c6c932917e92532c36d64785
Paul Le <l.nguyen.paul@gmail.com>
2022-01-08 22:14:36 -0500

    #45 Update backend to server frontend application

--- todo-app/server/index.js
+++ todo-app/server/index.js
@@ -32,6 +32,15 @@ app.use('/api/labels', labels);
 app.use('/api/auth', auth);
 app.use('/api/users', users);
 
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Static folder
+  app.use(express.static(__dirname + '/public/'));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
