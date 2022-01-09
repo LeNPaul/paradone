3ad69980ba4137a6a81710831b2570dc8166a961
Paul Le <l.nguyen.paul@gmail.com>
2022-01-09 00:42:14 -0500

    #45 Delete unused files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,5 @@
 
 cd /home/paradone/paradone;
 pm2 stop paradone;
-gulp clean;
 git pull;
-gulp build;
 pm2 start paradone;
--- scripts/start_instance.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo mongod --fork --logpath /var/log/mongodb/mongod.log --dbpath ~/data/db/;
-npm start;
