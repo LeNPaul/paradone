76339a48198be6bee5f01c530cdbebde3ac217d6
Paul Le <l.nguyen.paul@gmail.com>
2022-01-08 23:02:04 -0500

    #45 Remove if block and always server static files

--- todo-app/server/index.js
+++ todo-app/server/index.js
@@ -32,14 +32,11 @@ app.use('/api/labels', labels);
 app.use('/api/auth', auth);
 app.use('/api/users', users);
 
-// Handle production
-if (process.env.NODE_ENV === 'production') {
-  // Static folder
-  app.use(express.static(__dirname + '/public/'));
+// Static folder
+app.use(express.static(__dirname + '/public/'));
 
-  // Handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-}
+// Handle production SPA when built
+app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 const port = process.env.PORT || 7070;
 
