919a51bdbbbed92f2b15fe85d92ad9cb8c2c1c63
Paul Le <l.nguyen.paul@gmail.com>
2022-01-13 23:20:00 -0500

    #49 Add component for editing label and update header to show label edit modal

--- client/src/components/TheWorkspaceHeader.vue
+++ client/src/components/TheWorkspaceHeader.vue
@@ -4,6 +4,10 @@
     <button @click="toggleAddTask()" class="btn btn-link link-dark"><i class="fas fa-plus"></i></button>
     <button @click="toggleEditProject()" class="btn btn-link link-dark float-end"><i class="fas fa-ellipsis-v" v-show="this.$route.params.project"></i></button>
     <TheWorkspaceHeaderProjectEdit v-show="showEditProject" :project="this.$route.params.project"/>
+
+    <button @click="toggleEditLabel()" class="btn btn-link link-dark float-end"><i class="fas fa-ellipsis-v" v-show="this.$route.params.label"></i></button>
+    <TheWorkspaceHeaderLabelEdit v-show="showEditLabel" :label="this.$route.params.label"/>
+
   </header>
   <TheWorkspaceHeaderTaskAdd v-show="showAddTask" @close-add-task="toggleAddTask" @update-tasks="$emit('update-tasks')"/>
 </template>
@@ -11,6 +15,7 @@
 <script>
 import TheWorkspaceHeaderTaskAdd from '../components/TheWorkspaceHeaderTaskAdd'
 import TheWorkspaceHeaderProjectEdit from './TheWorkspaceHeaderProjectEdit'
+import TheWorkspaceHeaderLabelEdit from './TheWorkspaceHeaderLabelEdit'
 
 export default {
   name: 'TheWorkspaceHeader',
@@ -21,12 +26,14 @@ export default {
   },
   components: {
     TheWorkspaceHeaderTaskAdd,
-    TheWorkspaceHeaderProjectEdit
+    TheWorkspaceHeaderProjectEdit,
+    TheWorkspaceHeaderLabelEdit
   },
   data() {
     return {
       showAddTask: false,
-      showEditProject: false
+      showEditProject: false,
+      showEditLabel: false
     }
   },
   emits: ['update-tasks'],
@@ -36,11 +43,15 @@ export default {
     },
     toggleEditProject() {
       this.showEditProject = !this.showEditProject
+    },
+    toggleEditLabel() {
+      this.showEditLabel = !this.showEditLabel
     }
   },
   watch: {
     $route() {
       this.showEditProject = false
+      this.showEditLabel = false
       this.showAddTask = false
     }
   }
