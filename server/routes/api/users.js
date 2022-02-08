029907b27bfc443d39903f37758abe67c2fda50e
Paul Le <l.nguyen.paul@gmail.com>
2022-02-08 07:58:29 -0500

    #53 Add logic for updating a user's password in the database

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -69,32 +69,48 @@ router.post('/', (req, res) => {
 });
 
 router.put('/', (req, res) => {
-
   const { email, currentPassword, newPassword } = req.body;
-
   // Simple validation
   if(!email || !currentPassword || !newPassword) {
     return res.status(400).json({ msg: 'Please enter all fields' });
   }
-
   // Check for existing user
   User.findOne({ email })
     .then(user => {
       if(!user) return res.status(400).json({ msg: 'User does not exist' });
-
       // Validate password
       bcrypt.compare(currentPassword, user.password)
         .then(isMatch => {
           if(!isMatch) return res.status(400).json({ msg: 'Invalid password' });
-
-          console.log('User exists and currentPassword matches password in database')
-
+          user.password = newPassword
+          // Create salt & hash
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+              if(err) throw err;
+              user.password = hash;
+              user.save()
+              .then(user => {
+                jwt.sign(
+                  { id: user.id },
+                  config.get('jwtSecret'),
+                  { expiresIn: config.get('expiresIn') },
+                  (err, token) => {
+                    if(err) throw err;
+                    res.json({
+                      token,
+                      user: {
+                        id: user.id,
+                        username: user.username,
+                        email: user.email
+                      }
+                    });
+                  }
+                )
+              });
+            });
+          });
         })
-
     });
-
-  res.json({})
-
 });
 
 module.exports = router;
