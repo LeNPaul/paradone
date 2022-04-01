b493ba84367bad8078de6f5fa197cf9d24feb238
Paul Le <l.nguyen.paul@gmail.com>
2022-04-01 18:44:39 -0400

    #59 Add title parameter for blog post routes

--- blog/routes/blog.js
+++ blog/routes/blog.js
@@ -8,11 +8,11 @@ router.get('/', (req, res) => {
 
 // Inidividual blog posts
 router.get('/the-five-pillars-of-getting-things-done', (req, res) => {
-  res.render('posts/the-five-pillars-of-getting-things-done')
+  res.render('posts/the-five-pillars-of-getting-things-done', {title: 'The Five Pillars of Getting Things Done'})
 })
 
 router.get('/the-pomodoro-technique-the-life-changing-time-management-system-francesco-cirillo-book-summary', (req, res) => {
-  res.render('posts/the-pomodoro-technique-the-life-changing-time-management-system-francesco-cirillo-book-summary')
+  res.render('posts/the-pomodoro-technique-the-life-changing-time-management-system-francesco-cirillo-book-summary', {title: 'The Pomodoro Technique: The Life-Changing Time-Management System by Francesco Cirillo Book Summary'})
 })
 
 module.exports = router;
