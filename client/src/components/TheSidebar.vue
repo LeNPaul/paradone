e0f3ec0323060343365b96a933bf2103982669aa
Paul Le <l.nguyen.paul@gmail.com>
2022-04-28 15:12:40 -0700

    #61 Refactor sidebar from slide-out to static

--- client/src/components/TheSidebar.vue
+++ client/src/components/TheSidebar.vue
@@ -1,73 +1,63 @@
-<!-- https://www.codeply.com/p/J7fUOhLOy9 -->
 <template>
-  <div class="offcanvas offcanvas-start w-25" tabindex="-1" id="offcanvas" data-bs-keyboard="false" data-bs-backdrop="false">
-    <div class="offcanvas-header">
-      <h6 class="offcanvas-title d-none d-sm-block" id="offcanvas">Paradone</h6>
-      <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  <aside>
+    <div class="d-flex flex-column flex-shrink-0 p-3 bg-white border-end" style="width: 280px;">
+      <router-link to="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+        <i class="fas fa-home fs-5 me-1"></i>
+      </router-link>
+      <hr>
+      <ul class="nav nav-pills flex-column mb-auto">
+
+        <li class="nav-item">
+          <button @click="toggleAddProject" class="btn btn-link link-dark text-decoration-none"><span class="fs-6 me-2">Projects</span><i class="fas fa-plus"></i></button>
+          <TheSidebarProjectAdd v-show="showAddProject" @close-add-project="toggleAddProject" @update-projects="updateProjects"/>
+        </li>
+
+        <li :key='project.project_name' v-for="project in projects">
+          <router-link :to="{ name: 'Project', params: { project: project.project_name } }" class="nav-link link-dark">
+            <i class="fas fa-tasks me-1"></i>
+            {{ project.project_name }}
+          </router-link>
+        </li>
+
+        <li class="nav-item">
+          <button @click="toggleAddLabel" class="btn btn-link link-dark text-decoration-none"><span class="fs-6 me-2">Labels</span><i class="fas fa-plus"></i></button>
+          <TheSidebarLabelAdd v-show="showAddLabel" @close-add-label="toggleAddLabel" @update-labels="updateLabels"/>
+        </li>
+
+        <li :key='label.label_name' v-for="label in labels">
+          <router-link :to="{ name: 'Label', params: { label: label.label_name.toLowerCase().replace(/\s/g, '') } }" class="nav-link link-dark">
+            <i class="fas fa-tag me-1"></i>
+            {{ label.label_name }}
+          </router-link>
+        </li>
+
+      </ul>
+      <hr>
+      <div class="dropdown">
+        <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+          <i width="32" height="32" class="far fa-user me-2"></i>
+          <strong>{{ username }}</strong>
+        </a>
+        <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+          <li><router-link to="/resetpassword" class="dropdown-item">Reset Password</router-link></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><router-link to="/" @click="logout" class="dropdown-item" href="#">Sign out</router-link></li>
+        </ul>
+      </div>
     </div>
-    <div class="offcanvas-body px-0">
-      <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
-        <li class="nav-item">
-          <router-link class="nav-link text-truncate link-dark" to="/"><i class="fas fa-home fs-5"></i><span class="ms-1 d-none d-sm-inline">Home</span></router-link>
-        </li>
-      </ul>
-      <hr>
-      <button @click="toggleAddProject" class="btn btn-link link-dark text-decoration-none"><span class="fs-6 me-2">Projects</span><i class="fas fa-plus"></i></button>
-      <TheSidebarProjectAdd v-show="showAddProject" @close-add-project="toggleAddProject" @update-projects="updateProjects"/>
-      <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
-        <li :key='project.project_name' v-for="project in projects" class="nav-item">
-          <router-link :to="{ name: 'Project', params: { project: project.project_name } }" class="nav-link text-truncate link-dark"><i class="fas fa-arrow-right"></i><span class="ms-1 d-none d-sm-inline">{{ project.project_name }}</span></router-link>
-        </li>
-      </ul>
-      <hr>
-      <button @click="toggleAddLabel" class="btn btn-link link-dark text-decoration-none"><span class="fs-6 me-2">Labels</span><i class="fas fa-plus"></i></button>
-      <TheSidebarLabelAdd v-show="showAddLabel" @close-add-label="toggleAddLabel" @update-labels="updateLabels"/>
-      <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
-        <li :key='label.label_name' v-for="label in labels" class="nav-item">
-          <router-link :to="{ name: 'Label', params: { label: label.label_name.toLowerCase().replace(/\s/g, '') } }" class="nav-link text-truncate link-dark"><i class="fas fa-arrow-right"></i><span class="ms-1 d-none d-sm-inline">{{ label.label_name }}</span></router-link>
-        </li>
-      </ul>
-      <hr>
-      <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
-        <li class="nav-item">
-          <router-link class="nav-link text-truncate link-dark" to="/paradigm/eisenhower"><i class="fas fa-th-large"></i><span class="ms-1 d-none d-sm-inline">Eisenhower Matrix</span></router-link>
-        </li>
-      </ul>
-      <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
-        <li class="nav-item">
-          <router-link class="nav-link text-truncate link-dark" to="/paradigm/kanban"><i class="fas fa-columns"></i><span class="ms-1 d-none d-sm-inline">Kanban</span></router-link>
-        </li>
-      </ul>
-      <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
-        <li class="nav-item">
-          <router-link class="nav-link text-truncate link-dark" to="/paradigm/pomodoro"><i class="fas fa-clock"></i><span class="ms-1 d-none d-sm-inline">Pomodoro</span></router-link>
-        </li>
-      </ul>
-      <hr>
-
-      <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
-        <li class="nav-item">
-          <span class="nav-link text-truncate link-dark ms-1 d-none d-sm-inline text-muted">Logged in as {{ username }}</span>
-        </li>
-      </ul>
-
-      <ul class="nav nav-pills flex-column mb-sm-auto mb-0 mt-2 align-items-start" id="menu">
-        <li class="nav-item">
-          <router-link class="nav-link text-truncate link-dark" to="/resetpassword"><i class="fas fa-cog"></i><span class="ms-1 d-none d-sm-inline">Reset Password</span></router-link>
-        </li>
-      </ul>
-
-      <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
-        <li class="nav-item">
-          <router-link class="nav-link text-truncate link-dark" to="/" @click="logout"><i class="fas fa-sign-out-alt"></i><span class="ms-1 d-none d-sm-inline">Logout</span></router-link>
-        </li>
-      </ul>
-
-
-
-    </div>
-  </div>
+  </aside>
 </template>
+
+<style>
+aside {
+  display: flex;
+  flex-wrap: nowrap;
+  height: 100vh;
+  max-height: 100vh;
+  overflow-x: auto;
+  overflow-y: hidden;
+}
+</style>
 
 <script>
 import TheSidebarProjectAdd from '../components/TheSidebarProjectAdd'
