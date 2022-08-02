59703a12e840fb09c7cb4c64d1e5c35bdff72e03
Paul Le <l.nguyen.paul@gmail.com>
2022-08-01 20:59:00 -0400

    #66 Add error handling when attempting to sync with missing token settings

--- client/src/components/Sync.vue
+++ client/src/components/Sync.vue
@@ -1,12 +1,14 @@
 <template>
   <form @submit="onSubmit" class="form-control add-form mb-3 p-3">
     <div class="mb-3">
-      <input type="text" v-model="todoistToken" name="content" placeholder="Todoist API Token" class="form-control mb-3">
-      <input type="text" v-model="notionToken" name="content" placeholder="Notion API Token" class="form-control">
+      <h3>Sync Notion with Todoist</h3>
+      <p>Sync any tasks in Todoist with the label "notion" to any database in Notion shared with integration.</p>
     </div>
-    <button type="submit" class="btn btn-dark">Save</button>
+    <div v-show="errorMessage" class="alert alert-danger mb-3" role="alert">
+      {{ errorMessage }}
+    </div>
+    <button type="submit" class="btn btn-dark">Sync</button>
   </form>
-  <p :key='task.id' v-for="task in tasks">{{task.content}}</p>
 </template>
 
 <script>
@@ -16,16 +18,12 @@
       return {
         todoistToken: '',
         notionToken: '',
-        tasks: []
+        errorMessage: '',
       }
     },
     methods: {
       async onSubmit(e) {
         e.preventDefault()
-        /*if(!this.token) {
-          alert('Please add a task')
-          return
-        }*/
         const tokens = {
           notionToken: this.notionToken,
           todoistToken: this.todoistToken
@@ -39,9 +37,9 @@
           body: JSON.stringify(tokens)
         })
         const data = await res.json()
-        this.tasks = data
-        console.log(data)
-        return data
+        if (data.Error) {
+          this.errorMessage = data.Error
+        }
       }
     }
   }
