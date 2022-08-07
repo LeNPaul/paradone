0fd0495d4146fcbe9c559b778775c62560725203
Paul Le <l.nguyen.paul@gmail.com>
2022-08-07 18:08:33 -0400

    #66 Refactor code

--- server/routes/api/integration/sync.js
+++ server/routes/api/integration/sync.js
@@ -15,17 +15,14 @@ router.post('/', auth, async (req, res) => {
 
   // Get API token configuration and throw error is missing
   let settings = await Setting.find({user_id: req.user.id})
-
   if (!settings[0].notion_api_token || !settings[0].todoist_api_token) {
     res.status(404).json({'Error': 'Missing Notion or Todoist API token'})
   } else {
-
     let todoistRequestHeader = {
       headers: {
         'Authorization': 'Bearer ' + settings[0].todoist_api_token
       }
     }
-
     let notionRequestHeader = {
       headers: {
         'Authorization': 'Bearer ' + settings[0].notion_api_token,
@@ -33,74 +30,52 @@ router.post('/', auth, async (req, res) => {
         'Notion-Version': '2022-06-28'
       }
     }
-
-    // Get labels from Todoist
+    // Query Notion for database with "paradone" in the title and extract database ID
+    // TODO: remove hardcoding of "paradone"
+    const notionDatabaseResults = await axios.post('https://api.notion.com/v1/search', { 'query': 'paradone' }, notionRequestHeader);
+    let notionDatabaseId = notionDatabaseResults.data.results[0].id
+    // Query pages from database with "paradone" in the title
+    const notionDatabasePages = await axios.post('https://api.notion.com/v1/databases/' + notionDatabaseId + '/query', {}, notionRequestHeader);
+    let notionDatabasePageResults = notionDatabasePages.data.results
+    // For each Notion page, get the title property and put results in array with page ID
+    var notionPages = []
+    for ( let i = 0; i < notionDatabasePageResults.length; i++ ) {
+      const notionPageProperties = await axios.get('https://api.notion.com/v1/pages/' + notionDatabasePageResults[i].id + '/properties/title', notionRequestHeader);
+      notionPages.push({title: notionPageProperties.data.results[0].title.plain_text, id: notionDatabasePageResults[i].id})
+    }
+    // Get label_id for "notion" label in Todoist
+    // TODO: remove hardcoding of "notion" label name
     const todoistLabels = await axios.get('https://api.todoist.com/rest/v1/labels', todoistRequestHeader);
-    // Find the label_id for notion in Todoist
-    var notion_label_id
+    var notionLabelId
     for ( let i = 0; i < todoistLabels.data.length; i++ ) {
       if (todoistLabels.data[i].name == 'notion') {
-        notion_label_id = todoistLabels.data[i].id
+        notionLabelId = todoistLabels.data[i].id
       }
     }
-
-    const todoistTasks = await axios.get('https://api.todoist.com/rest/v1/tasks?label_id=' + notion_label_id, todoistRequestHeader);
-
-    // Query Notion for database IDs
-    const notionDatabaseIds = await axios.post('https://api.notion.com/v1/search',
-    {
-      'query': 'paradone'
-    },
-    notionRequestHeader);
-    let paradoneDatabaseId = notionDatabaseIds.data.results[0].id
-
-    // Get pages in Notion database
-    const databasePages = await axios.post('https://api.notion.com/v1/databases/' + paradoneDatabaseId + '/query',
-    {},
-    notionRequestHeader);
-
-    let databasePageResults = databasePages.data.results
-
-    var pageIds = []
-
-    // get title property
-    // For each result get the ttitle property and put in array
-    for ( let i = 0; i < databasePageResults.length; i++ ) {
-
-      const pageTitle = await axios.get('https://api.notion.com/v1/pages/' + databasePageResults[i].id + '/properties/title',
-      notionRequestHeader);
-
-      pageIds.push({title: pageTitle.data.results[0].title.plain_text, id: databasePageResults[i].id})
-    }
-
+    // Get tasks from Todoist with the "notion" label
+    const todoistTasks = await axios.get('https://api.todoist.com/rest/v1/tasks?label_id=' + notionLabelId, todoistRequestHeader);
+    // For each task from Todoist, if it does not already exist as a page in Notion then add as page to Notion database
     for ( let j = 0; j < todoistTasks.data.length; j++ ) {
-
-      const isFound = pageIds.some(element => {
+      const isFound = notionPages.some(element => {
         if (element.title === todoistTasks.data[j].content) {
           return true;
         }
         return false;
       });
-
       if (!isFound) {
         const notionCreatePageRes = await axios.post('https://api.notion.com/v1/pages',
         {
-          "parent": { "database_id": paradoneDatabaseId },
+          "parent": { "database_id": notionDatabaseId },
           "properties": {
       		"Name": {
       			"title": [ { "text": { "content": todoistTasks.data[j].content } } ]
       		 }
           }
-        },
-        notionRequestHeader);
+        }, notionRequestHeader);
       }
-
     }
-
-  res.send(todoistTasks.data)
-
+    res.json({'Success': 'Successfully synced at ' + Date().toString()})
   }
-
 });
 
 module.exports = router;
