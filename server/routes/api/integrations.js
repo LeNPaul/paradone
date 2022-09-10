61332164c00a8fc4e94ef86f7d4dee574b04f357
Paul Le <l.nguyen.paul@gmail.com>
2022-09-10 16:32:07 -0400

    #72 Add endpoint for adding new integration from user

--- server/routes/api/integrations.js
+++ server/routes/api/integrations.js
@@ -12,4 +12,22 @@ router.get('/', auth, async (req, res) => {
   res.send(await Integration.find({user_id: req.user.id}));
 });
 
+// @route  POST api/integrations
+// @desc   Save new user integration
+// @access Private
+router.post('/', auth, async (req, res) => {
+  const integration = {
+    user_id: req.user.id,
+    source: req.body.source,
+    is_active: true,
+    destination: req.body.destination,
+    source_query: req.body.query,
+    destination_modifier: req.body.modifier
+  }
+  const insertedIntegration = await Integration.create(integration);
+  console.log(insertedIntegration)
+  integration._id = insertedIntegration._id
+  res.status(200).send(integration);
+});
+
 module.exports = router;
