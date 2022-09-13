eed49b2bf5ff4b435c44c04a7c0d07352aa48384
Paul Le <l.nguyen.paul@gmail.com>
2022-09-13 14:06:40 -0400

    #74 Regularly query notion database ID using node-cron

--- server/index.js
+++ server/index.js
@@ -41,3 +41,51 @@ app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 const port = process.env.PORT || 7070;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// Run integration jobs
+const Integration = require('./models/Integration');
+const Setting = require('./models/Setting');
+const nodeCron = require("node-cron");
+const axios = require('axios');
+const job = nodeCron.schedule("* * * * * *", function sync() {
+  Integration.find({ is_active: true })
+    .then(integrations => {
+      // For each integration, find user settings using userID
+      for (let i = 0 ; i < integrations.length ; i++) {
+        let settings = Setting.find({user_id: integrations[i].user_id}).then(async settings => {
+          if(settings[0].notion_api_token && settings[0].todoist_api_token) {
+            let todoistRequestHeader = {
+              headers: {
+                'Authorization': 'Bearer ' + settings[0].todoist_api_token
+              }
+            }
+            let notionRequestHeader = {
+              headers: {
+                'Authorization': 'Bearer ' + settings[0].notion_api_token,
+                'Content-Type': 'application/json',
+                'Notion-Version': '2022-06-28'
+              }
+            }
+            console.log(todoistRequestHeader)
+            console.log(notionRequestHeader);
+            // Query Notion for database with "req.body.notionDatabase" in the title and extract database ID
+            let notionDatabaseResults
+            try {
+              notionDatabaseResults = await axios.post('https://api.notion.com/v1/search', { 'query': integrations[i].destination_modifier }, notionRequestHeader);
+            } catch (error) {
+              console.log({'Error': error.response.data.message})
+              return
+            }
+            let notionDatabaseId
+            if (notionDatabaseResults.data.results.length == 0) {
+              console.log({'Error': 'Notion - Database with name ' + integrations[i].destination_modifier + ' not found.' })
+              return
+            } else {
+              notionDatabaseId = notionDatabaseResults.data.results[0].id
+            }
+            console.log(notionDatabaseId)
+          }
+        })
+      }
+    })
+});
