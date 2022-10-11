ff83da5340c585fc47c6186ddbc876b47a2d508d
Paul Le <l.nguyen.paul@gmail.com>
2022-10-11 23:57:29 +0200

    #77 Remove old endpoint for sync Notion and Todoist

--- server/index.js
+++ server/index.js
@@ -22,13 +22,11 @@ mongoose.connect(db)
 // Routes
 const auth = require('./routes/api/auth');
 const users = require('./routes/api/users');
-const sync = require('./routes/api/integration/sync');
 const settings = require('./routes/api/settings');
 const integrations = require('./routes/api/integrations');
 
 app.use('/api/auth', auth);
 app.use('/api/users', users);
-app.use('/api/sync', sync);
 app.use('/api/settings', settings);
 app.use('/api/integrations', integrations);
 
