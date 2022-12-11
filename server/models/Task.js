63623bf7c7a2deb16075505e25d214205c1fdb2c
Paul Le <l.nguyen.paul@gmail.com>
2022-12-11 15:17:20 -0500

    #77 Add database model for tasks

---
+++ server/models/Task.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+// Use the document ID as the task_id
+const TaskSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  user_id: {
+    type: String,
+    required: true
+  },
+  parent_ids: {
+    type: Array
+  },
+  tag_ids: {
+    type: Array
+  },
+  due_datetime: {
+    type: Date
+  },
+  completed: {
+    type: Boolean
+  }
+});
+
+module.exports = Task = mongoose.model('task', TaskSchema);
