f00b3aa3e598c3a88000b1f9c8b0e0ac1afa3f63
Paul Le <l.nguyen.paul@gmail.com>
2022-12-17 00:31:51 -0500

    #82 Remove parent task when task is clicked on

--- client/src/components/TheWorkspaceHeaderTaskAdd.vue
+++ client/src/components/TheWorkspaceHeaderTaskAdd.vue
@@ -5,7 +5,7 @@
       <textarea type="text" v-model="content" name="content" placeholder="Content" class="form-control border-0 shadow-none" rows="3"></textarea>
     </div>
     <button type="submit" class="btn btn-dark">Save</button>
-    <button :key='task.title' v-for="task in parentTasks" type="button" class="btn btn-outline-dark p-1 ms-2 px-2" disabled>{{ task.title }}</button>
+    <button :key='task.title' v-for="task in parentTasks" type="button" class="btn btn-outline-dark p-1 ms-2 px-2" @click="this.parentTasks=this.parentTasks.filter(parentTask=>parentTask._id!=task._id)">{{ task.title }}</button>
     <button class="btn btn-outline-dark btn-sm float-end" type="button" data-bs-toggle="dropdown"><i class="fas fa-list"></i></button>
     <ul class="dropdown-menu">
         <li :key='task.title' v-for="task in tasks" v-show="!this.parentTasks.some(parentTask=>parentTask._id==task._id)" @click="this.parentTasks.push({_id: task._id, title: task.title})"><a class="dropdown-item">{{ task.title }}</a></li>
