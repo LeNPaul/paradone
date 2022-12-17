39de3c718c764a1b95fb8db2971acf57c243c9d7
Paul Le <l.nguyen.paul@gmail.com>
2022-12-17 00:20:53 -0500

    #82 Update task add component to add parent tasks when adding new task

--- client/src/components/TheWorkspaceHeaderTaskAdd.vue
+++ client/src/components/TheWorkspaceHeaderTaskAdd.vue
@@ -5,6 +5,11 @@
       <textarea type="text" v-model="content" name="content" placeholder="Content" class="form-control border-0 shadow-none" rows="3"></textarea>
     </div>
     <button type="submit" class="btn btn-dark">Save</button>
+    <button :key='task.title' v-for="task in parentTasks" type="button" class="btn btn-outline-dark p-1 ms-2 px-2" disabled>{{ task.title }}</button>
+    <button class="btn btn-outline-dark btn-sm float-end" type="button" data-bs-toggle="dropdown"><i class="fas fa-list"></i></button>
+    <ul class="dropdown-menu">
+        <li :key='task.title' v-for="task in tasks" v-show="!this.parentTasks.some(parentTask=>parentTask._id==task._id)" @click="this.parentTasks.push({_id: task._id, title: task.title})"><a class="dropdown-item">{{ task.title }}</a></li>
+    </ul>
   </form>
 </template>
 
@@ -14,9 +19,16 @@
     data() {
       return {
         title: null,
-        content: null
+        content: null,
+        parentTasks: []
       }
     },
+    props: {
+    tasks: {
+      type: Array,
+      required: true
+    }
+  },
     emits: ['update-tasks', 'close-add-task'],
     methods: {
       async onSubmit(e) {
@@ -27,7 +39,8 @@
         }
         const newTask = {
           title: this.title,
-          content: this.content
+          content: this.content,
+          parent_ids: this.parentTasks.map((task)=>task._id)
         }
         const res = await fetch('/api/tasks', {
           method: 'POST',
@@ -41,6 +54,7 @@
         this.$emit('update-tasks')
         this.title = null
         this.content = null
+        this.parentTasks = []
         return data
       }
     }
