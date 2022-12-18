fcc4e611345e54b265518466cefb6f401c7e0382
Paul Le <l.nguyen.paul@gmail.com>
2022-12-18 15:53:46 -0500

    #83 Refactor requests module and simplify header argument

--- client/src/assets/js/requests.js
+++ client/src/assets/js/requests.js
@@ -1,34 +1,29 @@
+let header = {
+  'Content-type': 'application/json',
+  'x-auth-token': localStorage.getItem('token') || ''
+}
+
 let fetchTask = async function(id) {
   const res = await fetch(`/api/tasks/${id}`, {
-    headers: {
-      'x-auth-token': localStorage.getItem('token') || ''
-    }
+    headers: header
   })
-  const data = await res.json()
-  return data
+  return await res.json()
 }
 
 let fetchTasks = async function() {
   const res = await fetch('/api/tasks', {
-    headers: {
-      'x-auth-token': localStorage.getItem('token') || ''
-    }
+    headers: header
   })
-  const data = await res.json()
-  return data
+  return await res.json()
 }
 
 let createTask = async function(newTask) {
   const res = await fetch('/api/tasks', {
     method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-      'x-auth-token': localStorage.getItem('token') || ''
-    },
+    headers: header,
     body: JSON.stringify(newTask),
   })
-  const data = await res.json()
-  return data
+  return await res.json()
 }
 
 exports.fetchTask = fetchTask;
