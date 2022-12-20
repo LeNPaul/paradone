3a521394fdba42b9bdd79f67dd47920919b01eed
Paul Le <l.nguyen.paul@gmail.com>
2022-12-19 23:46:26 -0500

    #84 Hide completed tasks in parent dropdown in add task component

--- client/src/components/TheWorkspaceTaskAdd.vue
+++ client/src/components/TheWorkspaceTaskAdd.vue
@@ -8,7 +8,7 @@
     <button :key='task.title' v-for="task in parentTasks" type="button" class="btn btn-outline-dark p-1 ms-2 px-2" @click="this.parentTasks=this.parentTasks.filter(parentTask=>parentTask._id!=task._id)">{{ task.title }}</button>
     <button class="btn btn-outline-dark btn-sm float-end" type="button" data-bs-toggle="dropdown"><i class="fas fa-list"></i></button>
     <ul class="dropdown-menu">
-        <li :key='task.title' v-for="task in tasks" v-show="!this.parentTasks.some(parentTask=>parentTask._id==task._id)" @click="this.parentTasks.push({_id: task._id, title: task.title})"><a class="dropdown-item">{{ task.title }}</a></li>
+        <li :key='task.title' v-for="task in tasks" v-show="!this.parentTasks.some(parentTask=>parentTask._id==task._id) && !task.completed" @click="this.parentTasks.push({_id: task._id, title: task.title})"><a class="dropdown-item">{{ task.title }}</a></li>
     </ul>
   </form>
 </template>
