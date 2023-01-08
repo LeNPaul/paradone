2f405898de61c8416da5c240261e97b04980b21b
Paul Le <l.nguyen.paul@gmail.com>
2023-01-08 16:15:47 -0500

    #85 Add tasks as nodes in graph view

--- client/src/views/Graph.vue
+++ client/src/views/Graph.vue
@@ -49,17 +49,8 @@ export default {
     return {
       tasks: [],
       showModal: false,
-      nodes: {
-        node1: { name: "Node 1" },
-        node2: { name: "Node 2" },
-        node3: { name: "Node 3" },
-        node4: { name: "Node 4" },
-      },
-      edges: {
-        edge1: { source: "node1", target: "node2" },
-        edge2: { source: "node2", target: "node3" },
-        edge3: { source: "node3", target: "node4" },
-      },
+      nodes: {},
+      edges: {},
       configs: {
         node: {
           normal: {
@@ -83,6 +74,9 @@ export default {
   methods: {
     async updateTasks() {
       this.tasks = await requests.fetchTasks()
+      this.tasks.forEach((task)=>{
+        this.nodes[task._id] = {name: task.title}
+      })
     }
   },
   async created() {
