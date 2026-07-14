634ff04bf3569a7f928dd0e49fc39ed4a59ee9b8
Paul Le <l.nguyen.paul@gmail.com>
2026-07-14 00:37:12 -0400

    Remove old project files

--- client/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import VNetworkGraph from "v-network-graph"
-
-createApp(App).use(store).use(router).use(VNetworkGraph).mount('#app')
